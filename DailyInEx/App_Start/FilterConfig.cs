813d2836ca3d9d00c88b4fa1a91a950900d2c222
Naime Ahmed <naime1947@gmail.com>
2019-05-23 06:25:38 +0600

    Add project files.

---
+++ DailyInEx/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DailyInEx
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
